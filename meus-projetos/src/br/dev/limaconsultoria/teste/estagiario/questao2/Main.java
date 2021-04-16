7cf2d83400fa2c7f607d7f6e08fe5db0e86227d4
JOSE0193 <63592803+JOSE0193@users.noreply.github.com>
2021-04-16 14:16:27 -0300

    Update Main.java
    Correção do exercício 2

--- meus-projetos/src/br/dev/limaconsultoria/teste/estagiario/questao2/Main.java
+++ meus-projetos/src/br/dev/limaconsultoria/teste/estagiario/questao2/Main.java
@@ -1,18 +1,17 @@
 package br.dev.limaconsultoria.teste.estagiario.questao2;
 
 public class Main {
-
-	public static void main(String[] args) {
-		
-	int mult = 400;
 	
-	for (int i = 0; i<= 4599; i++) {
-		mult++;
-		if (mult %3 ==0) {
-		System.out.println(mult);
-		}
+	public static void main(String[] args) {
+		int mult = 400;
+		int [] fim = new int[4200];
+			
+		for (int i = 0; i<= fim.length ; i++) {
+			mult++;
+			if (mult %3 ==0) {
+				System.out.println(mult);
+		
+				}
+			}
 	}
-
-	}
-
 }
